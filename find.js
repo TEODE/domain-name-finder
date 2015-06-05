e62f6467fe3ce01714d6ba888650c407aaf469e1
Vincent Giraud <vincent.giraud@teode.com>
2015-06-05 12:24:08 +0800

    Load balance availibility check onto 2 services

--- find.js
+++ find.js
@@ -11,18 +11,28 @@ var cr = new Crawler({
     // At the end
     onDrain: function() {
         console.log("<End of processing>");
-        Say.speak('Deranged', 'That\'s all, folks!');
-        process.exit();
+        // Let finish the last piece of speech
+        setTimeout(function(){
+            Say.speak('Deranged', 'That\'s all, folks!');
+            process.exit();
+        }, 4000);
     },
     // This will be called for each crawled page
     callback : function (error, result, $) {
         var domain=result.uri.substring(result.uri.lastIndexOf("=")+1,result.uri.lastIndexOf("."));
-        if (result.body == -1) {
+
+        // TODO: Abstract this out to a stragegy pattern for different services response to be parsed
+        if (result.body.indexOf("ComAvailable")!= -1) { // domaintyper.com service parsing
+            result.body = JSON.parse(result.body);
+            result.body = (result.body.ComAvailable) ? -1 : "Taken";
+        }
+
+        if (result.body === -1) {
             console.log(domain + ".com is available!");
             // Try to reduce overlap of speech even though asynchronous
             setTimeout(function(){
                 Say.speak((Math.random() < 0.5 ? 'Victoria':'Alex'), domain);
-            }, 3000);
+            }, 2000);
             if (domain_options.withThesaurus) {
                 var thesaurusArray = th.find(domain);
                 if (thesaurusArray.length > 0) {
@@ -49,5 +59,11 @@ var domain_options = {length:5, withThesaurus:true};
  */
 for (var i = 0; i < chances; i++) {
     var domain_candidate = ch.word(domain_options);
-    cr.queue("http://www.domainnamesoup.com/cell.php?domain="+domain_candidate+".com");
+    // Alternate randomly between different services
+    // TODO: Abstract this into a separate Adapter class essentially to load balance and avoid IP banning if any
+    if (Math.random() < 0.5) {
+        cr.queue("http://www.domainnamesoup.com/cell.php?domain=" + domain_candidate + ".com");
+    } else {
+        cr.queue("https://domaintyper.com/DomainCheckHandler.ashx?domain=" + domain_candidate + ".com");
+    }
 }
